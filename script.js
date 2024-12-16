5076917ac8431374a7df53f21baa345c5f6f91bb
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2024-12-16 20:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // your code here
+
+document.getElementById("button").addEventListener("click", function(event){
+  event.preventDefault()
+	let name = document.getElementById("name").value;
+	let year =document.getElementById("year").value;
+
+	let display= document.getElementById("display");
+	let ans="https://localhost:8080/";
+
+	if(name || year)
+	{
+		ans+="?";
+		let added=false;
+		if(name )
+		{
+			added=true;
+			ans+="name="+name;
+		}
+		if(year)
+		{
+			if(added)
+			{
+				ans+="&";
+			}
+			ans+="year="+year;
+		}
+	}
+
+	display.innerText=ans;
+	
+});
+function generateLink() {
+
+	
+
+	
+	
+}
